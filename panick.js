ec9ba81abb7d6ae174ce830ff2cb09d517d0a3e2
ItsInqz <88752410+ItsInqz@users.noreply.github.com>
2022-05-12 10:45:25 -0400

    added functioning panick to panick.js

--- panick.js
+++ panick.js
@@ -1,1 +1,5 @@
 console.log("Panick Loaded...")
+
+document.querySelector("link[rel='shortcut icon']").href = "https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico";
+document.querySelector("link[rel*='icon']").href = "https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico";
+document.title = "Schoology"
