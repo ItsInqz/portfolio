412e0abfb7eaa8f89358cbf60dba4460f35b21c6
ItsInqz <88752410+ItsInqz@users.noreply.github.com>
2022-05-12 10:52:59 -0400

    fixed testscript

--- panick.js
+++ panick.js
@@ -1,5 +1,7 @@
-console.log("Panick Loaded...")
 
-document.querySelector("link[rel='shortcut icon']").href = "https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico";
-document.querySelector("link[rel*='icon']").href = "https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/favicon.ico";
-document.title = "Schoology"
+
+javascript: (() => {
+    document.querySelector("link[rel='shortcut icon']").href = "https://mbca.schoology.com/sites/all/themes/schoology_theme/favicon.ico";
+    document.querySelector("link[rel*='icon']").href = "https://mbca.schoology.com/sites/all/themes/schoology_theme/favicon.ico";
+    document.title = "Schoology"
+})();
